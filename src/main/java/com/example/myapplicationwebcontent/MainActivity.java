e35d29c154aaad07a103dbe32a82d4e2abbe3c77
kratos <vovan198080@inbox.ru>
2020-04-15 16:17:15 +0300

    Initional Project

--- src/main/java/com/example/myapplicationwebcontent/MainActivity.java
+++ src/main/java/com/example/myapplicationwebcontent/MainActivity.java
@@ -7,12 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String mailRu = "https://mail.ru/";
+    private String google = "https://www.google.ru/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Hello", mailRu);
+        Log.i("Hello", google);
     }
 }
