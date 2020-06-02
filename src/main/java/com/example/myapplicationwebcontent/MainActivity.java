afac6d04624c7af554a7241783a79e0be197cd3c
kratos <vovan198080@inbox.ru>
2020-06-02 16:05:15 +0300

    Получение картинок с интернета

--- src/main/java/com/example/myapplicationwebcontent/MainActivity.java
+++ src/main/java/com/example/myapplicationwebcontent/MainActivity.java
@@ -66,3 +66,56 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+//=================================================================================================
+//код для получения картинок с интернета
+//public class MainActivity extends AppCompatActivity {
+//
+//    private ImageView imageView;
+//    private String url = "https://avatars.mds.yandex.net/get-pdb/2821631/820e19d2-7a17-41a9-8ae4-f7b221cca8d5/s1200";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        imageView = findViewById(R.id.imageView);
+//    }
+//
+//
+//    public void onClickDownloadImage(View view) {
+//        DownloadImageTask task = new DownloadImageTask();
+//        Bitmap bitmap = null;
+//        try {
+//            bitmap = task.execute(url).get();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        imageView.setImageBitmap(bitmap);
+//    }
+//
+//    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+//        @Override
+//        protected Bitmap doInBackground(String... strings) {
+//            URL url = null;
+//            HttpURLConnection urlConnection = null;
+//            try {
+//                url = new URL(strings[0]);
+//                urlConnection = (HttpURLConnection) url.openConnection();
+//                InputStream inputStream = urlConnection.getInputStream();
+//                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+//                return bitmap;
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (urlConnection != null) {
+//                    urlConnection.disconnect();
+//                }
+//            }
+//            return null;
+//        }
+//    }
+//}
